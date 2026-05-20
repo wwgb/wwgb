3239ddb07bbda5aaf3bbf82ed684985702081c74
Hlu Junh <43178220+wwgb@users.noreply.github.com>
2026-05-20 09:29:52 +0800

    Update update-game.js

--- update-game.js
+++ update-game.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const isWin = Math.random() < 0.5;
 const items = ['🧻 纸巾', '🥕 胡萝卜'];
 const randomItem = items[Math.floor(Math.random() * 2)];
-const result = isWin ? '🎉 蒸蚌！' : `😿 没抓到，今天是：${randomItem}`;
+let result = isWin ? '🎉 蒸蚌！' : `😿 没抓到，今天是：${randomItem}`;
 let readme = fs.readFileSync('README.md', 'utf8');
 readme = readme.replace('<!-- GAME_RESULT -->', result);
 fs.writeFileSync('README.md', readme);
